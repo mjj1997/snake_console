47333c4c85f124264f3b521cc7a0efae779ea605
Ma Jiajun <691460087@qq.com>
2025-04-19 16:48:01 +0800

    feat: add play() to GameView class

--- src/game_view.cpp
+++ src/game_view.cpp
@@ -13,3 +13,32 @@ bool GameView::loadPlayDataFromFile(const std::string& fileName)
 
     return m_controller.loadPlayDataFromFile(fin);
 }
+
+void GameView::play()
+{
+    while (true) {
+        // 系统调用：清屏
+        system("cls");
+        //输出游戏界面：棋盘
+        printMatrix();
+
+        std::cout << "direction: W(up) A(left) S(down) D(right)\n"
+                  << "$: food\n"
+                  << "@: snake head\n"
+                  << "#: snake tail\n";
+
+        //输入一个方向，表示蛇要往这个方向前进一步
+        char direction;
+        std::cin >> direction;
+        //往前走一步，如果判断无法往前走到用户指定的位置，就退出程序（比如，蛇咬到了自己、撞到了墙壁）
+        if (!m_controller.goAhead(direction)) {
+            std::cout << "Game Over!" << std::endl;
+            break;
+        }
+    }
+}
+
+// TODO: complete this function
+void GameView::printMatrix() const
+{
+}
--- src/game_view.h
+++ src/game_view.h
@@ -9,6 +9,17 @@ class GameView
 public:
     // 从文件中加载界面数据，存放到内部容器中，再根据容器内容绘制界面
     bool loadPlayDataFromFile(const std::string& fileName);
+    // 开始游戏
+    void play();
+
+private:
+    // 打印贪吃蛇游戏，将界面和内存之间的数据对应起来
+    // 界面     内存
+    // '_'  <== '0'
+    // '$'  <== '2'
+    // '#'  <== '1'
+    // '@'  <== '1'
+    void printMatrix() const;
 
 private:
     GameController m_controller;
