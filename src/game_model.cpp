c3f720b73172b28c2e69621d4749c4927ade0403
Ma Jiajun <691460087@qq.com>
2025-04-21 10:35:31 +0800

    feat: add getPlayBoardCell() to GameModel class

--- src/game_model.cpp
+++ src/game_model.cpp
@@ -15,3 +15,7 @@ void GameModel::addRow(const vector<char>& row)
     m_playBoard.push_back(row);
 }
 
+char GameModel::getPlayBoardCell(int row, int col) const
+{
+    return m_playBoard.at(row).at(col);
+}
--- src/game_model.h
+++ src/game_model.h
@@ -10,5 +10,6 @@ public:
     void addRow(const std::vector<char>& row);
 
 private:
+    char getPlayBoardCell(int row, int col) const;
     std::vector<std::vector<char>> m_playBoard; // 整个游戏的数据（二维数组）
 };
