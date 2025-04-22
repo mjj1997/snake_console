6b1e6835b09aef03c9a5c1182d390725a2bb6ec1
Ma Jiajun <691460087@qq.com>
2025-04-22 17:27:07 +0800

    build: build and run game

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "game_view.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    //创建游戏对象
+    GameView snake;
+    //读取文本文件，加载游戏数据
+    if (!snake.loadPlayDataFromFile("data/map.txt")) {
+        cout << "loadPlayDataFromFile data.txt failed!" << endl;
+        return 1;
+    }
+
+    snake.play(); //整个游戏都在这个函数里
+
+    return 0;
+}
